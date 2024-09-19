23f2de0c279ea11e3cdac72932603b62e158f332
NomanNayeem5516 <nomannayeem5516@gmail.com>
2024-09-19 14:40:10 +0600

    mvvm clean arc formate

--- lib/network_manager/http_helper.dart
+++ lib/network_manager/http_helper.dart
@@ -1,7 +1,10 @@
 
 import 'dart:convert';
 
+
+
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 import 'dart:io';
 
 import 'package:mvvm_arc_with_http/utils/print_value.dart';
@@ -59,7 +62,87 @@ class HttpHelper{
     }
   }
 
+  ///Put Api
+  Future<dynamic>put({required String url,Object? requestBody,  bool isRequireAuthorization=false}) async{
+    Map<String,String>apiHeaders={"Contact Type":"application/json"};
+    if(isRequireAuthorization){
+      apiHeaders={
+        "Contact Type":"application/json",
+        "CAuthorization":'Bearer"userBearerToken"'
+      };
+    }
+    try{
+      http.Response apiResponse;
+      if(requestBody==null){
 
+        apiResponse=await http.put(Uri.parse(url),headers: apiHeaders);
+      }else{
+        apiResponse=await http.put(Uri.parse(url),body: jsonEncode(requestBody),headers: apiHeaders);
+      }
+
+      printValue(url,tag: "API GET URL");
+      printValue(apiHeaders,tag: "API HEADER");
+      printValue(apiResponse,tag: "API RESPONSE");
+      return _returnResponse(response:apiResponse);
+
+    }on SocketException{
+      return null;
+    }
+  }
+
+  ///Delete Api
+  Future<dynamic>delete({required String url,Object? requestBody,  bool isRequireAuthorization=false}) async{
+    Map<String,String>apiHeaders={"Contact Type":"application/json"};
+    if(isRequireAuthorization){
+      apiHeaders={
+        "Contact Type":"application/json",
+        "CAuthorization":'Bearer"userBearerToken"'
+      };
+    }
+    try{
+      http.Response apiResponse;
+      if(requestBody==null){
+
+        apiResponse=await http.delete(Uri.parse(url),headers: apiHeaders);
+      }else{
+        apiResponse=await http.delete(Uri.parse(url),body: jsonEncode(requestBody),headers: apiHeaders);
+      }
+
+      printValue(url,tag: "API GET URL");
+      printValue(apiHeaders,tag: "API HEADER");
+      printValue(apiResponse,tag: "API RESPONSE");
+      return _returnResponse(response:apiResponse);
+
+    }on SocketException{
+      return null;
+    }
+  }
+
+  ///Multiple Api
+
+  Future<dynamic> uploadImage({required File imageFile,required String url}) async{
+    try{
+      var request = http.MultipartRequest('POST', Uri.parse(url));
+      request.headers.addAll({"Contact Type":"multipart/form-data"});
+      request.files.add(
+        await http.MultipartFile.fromPath(
+          'file',
+          imageFile.path,
+          filename: imageFile.toString().split("/").last
+        )
+      );
+      StreamedResponse streamedResponse = await request.send();
+      http.Response response= await http.Response.fromStream(streamedResponse);
+
+      printValue(url,tag: "API MULTIPLE URL:",);
+      printValue({"Contact Type":"multipart/form-data"},tag: "API Header:",);
+      printValue(response.body,tag: "API RESPONSE:",);
+
+      return _returnResponse(response:response);
+    }on SocketException{
+      return null;
+    }
+  }
 
 }
 
